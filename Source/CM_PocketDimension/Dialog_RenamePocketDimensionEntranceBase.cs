9d7516d5c3ec2360692d4f94c38e805ee2eaf50a
AlexisPopcorn <101859265+AlexisPopcorn@users.noreply.github.com>
2024-06-01 00:49:20 -0500

    Add files via upload

---
+++ Source/CM_PocketDimension/Dialog_RenamePocketDimensionEntranceBase.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using Verse;
+using LudeonTK;
+
+namespace KB_PocketDimension
+{
+    class Dialog_RenamePocketDimensionEntranceBase : Dialog_Rename<IRenameable>
+    {
+        private string buildingSeed;
+         
+        public Dialog_RenamePocketDimensionEntranceBase(Building_PocketDimensionEntranceBase building) : base((IRenameable)null)
+        {
+            this.buildingSeed = building.dimensionSeed;
+            curName = building.uniqueName ?? building.LabelNoCount;
+        }
+        protected override void OnRenamed(string name)
+        {
+            Building_PocketDimensionBox box = PocketDimensionUtility.GetBox(buildingSeed);
+            Building_PocketDimensionExit exit = PocketDimensionUtility.GetExit(buildingSeed);
+
+            if (box != null)
+                box.uniqueName = curName;
+            if (exit != null)
+                exit.uniqueName = "KB_PocketDimension_ExitName".Translate(curName);
+
+            Messages.Message("KB_RenamePocketDimensionMessage".Translate(curName), MessageTypeDefOf.TaskCompletion);
+        }
+    }
+}
